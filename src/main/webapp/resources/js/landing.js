31fed6843946a9ab75745dd759701bd6a87dcaa3
airofsummer <vulo4ka@gmail.com>
2015-01-15 18:34:57 +0200

    added sign up page, fixed some stylish issues, added behavior for some elements

--- src/main/webapp/resources/js/landing.js
+++ src/main/webapp/resources/js/landing.js
@@ -2,8 +2,38 @@
  * Created by Airofsummer on 14.01.2015.
  */
     $(document).ready(function(){
-        $( "#signpop" ).dialog({ autoOpen: false });
+        $( "#closepop").hide();
+        $( "#signpop" ).dialog({
+            autoOpen: false,
+            minHeight: 400,
+            minWidth: 300,
+            height: '30%',
+            width: '25%',
+            modal: true,
+            position: ['middle',20],
+            hide: 150,
+            show: 150
+
+        });
+        $( "#regpop" ).dialog({
+            autoOpen: false,
+            minHeight: 400,
+            minWidth: 300,
+            height: '30%',
+            width: '25%',
+            modal: true,
+            position: ['middle',20],
+            hide: 150,
+            show: 150
+        });
         $( "#signin" ).click(function() {
             $( "#signpop" ).dialog( "open" );
+            $( "#closepop" ).fadeIn(50);
         });
+        $( "#closepop" ).click(function() {
+            $( "#signpop" ).dialog( "close" );
+            $( "#regpop" ).dialog( "close" );
+            $( "#closepop" ).fadeOut(50);
+        });
+
     });
