cf3dce1553f8bf6debd87df76aec04890dacb9bd
airofsummer <vulo4ka@gmail.com>
2015-01-21 09:42:24 +0200

    demo

--- src/main/webapp/resources/js/landing.js
+++ src/main/webapp/resources/js/landing.js
@@ -1,33 +1,47 @@
 /**
  * Created by Airofsummer on 14.01.2015.
  */
-angular.module('LandApp', ['ui.bootstrap']);
-angular.module('LandApp').controller('ModalCtrl', function ($scope, $modal, $log) {
+    angular.module('LandApp', ['ui.bootstrap', "ui.router", "ngAnimate"]);
+    angular.module('LandApp').controller('ModalCtrl', function ($scope, $modal) {
 
-    $scope.items = ['item1', 'item2', 'item3'];
+        $scope.open = function (size) {
 
-    $scope.open = function (size) {
+            var modalInstance = $modal.open({
+                templateUrl: 'myModal.html',
+                controller: 'ModalInstanceCtrl',
+                size: size,
+                resolve: { },
+                backdrop: true,
+                backdropClass: 'backdrop',
+                windowClass: 'fuckshadows'
+            });
 
-        var modalInstance = $modal.open({
-            templateUrl: 'myModal.html',
-            controller: 'ModalInstanceCtrl',
-            size: size,
-            resolve: { },
-            backdrop: true,
-            backdropClass: 'backdrop',
-            windowClass: 'fuckshadows'
-        });
+        };
+        $scope.master = {};
 
-    };
-});
+        $scope.check = function(user) {
+            $scope.master = angular.copy(user);
+        };
+    });
 
-angular.module('LandApp').controller('ModalInstanceCtrl', function ($scope, $modalInstance) {
+    angular.module('LandApp').controller('ModalInstanceCtrl', function ($scope, $modalInstance) {
+        $scope.ok = function () {
+            $modalInstance.close();
+        };
+    });
 
-    $scope.ok = function () {
-        $modalInstance.close();
-    };
-    $scope.cancel = function () {
-        $modalInstance.dismiss('cancel');
-    };
+    angular.module('LandApp').config(function($stateProvider, $urlRouterProvider){
 
-});
+        $stateProvider
+            .state("foo", {
+                url: "/foo",
+                template: '<div ng-include="\'/pages/sign_up.html\'"></div>'
+            })
+            .state("bar", {
+                url: "/bar",
+                template: '<div id="signpop" ng-include="\'/pages/sign_in.html\'"></div>'
+            });
+
+        $urlRouterProvider.otherwise("/bar");
+    });
+
