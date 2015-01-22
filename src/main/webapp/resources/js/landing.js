2034d64961bb010bf62caa7e758b87590bf07db2
airofsummer <vulo4ka@gmail.com>
2015-01-22 18:05:09 +0200

    demo

--- src/main/webapp/resources/js/landing.js
+++ src/main/webapp/resources/js/landing.js
@@ -2,7 +2,7 @@
  * Created by Airofsummer on 14.01.2015.
  */
     angular.module('LandApp', ['ui.bootstrap', "ui.router", "ngAnimate"]);
-    angular.module('LandApp').controller('ModalCtrl', function ($scope, $modal) {
+    angular.module('LandApp').controller('ModalCtrl',function ($scope, $modal) {
 
         $scope.open = function (size) {
 
@@ -17,31 +17,34 @@
             });
 
         };
-        $scope.master = {};
 
-        $scope.check = function(user) {
-            $scope.master = angular.copy(user);
-        };
     });
+    angular.module('LandApp').controller('Validation', ['$scope', function($scope) {
+    $scope.master = {};
 
+    $scope.check = function(user) {
+        $scope.master = angular.copy(user);
+    };
+
+}]);
     angular.module('LandApp').controller('ModalInstanceCtrl', function ($scope, $modalInstance) {
         $scope.ok = function () {
             $modalInstance.close();
         };
     });
 
-    angular.module('LandApp').config(function($stateProvider, $urlRouterProvider){
-
-        $stateProvider
-            .state("foo", {
-                url: "/foo",
-                template: '<div ng-include="\'/pages/sign_up.html\'"></div>'
-            })
-            .state("bar", {
-                url: "/bar",
-                template: '<div id="signpop" ng-include="\'/pages/sign_in.html\'"></div>'
-            });
-
-        $urlRouterProvider.otherwise("/bar");
-    });
+    //angular.module('LandApp').config(function($stateProvider, $urlRouterProvider){
+    //
+    //    $stateProvider
+    //        .state("foo", {
+    //            url: "/foo",
+    //            template: '<div ng-include="\'/pages/sign_up.html\'"></div>'
+    //        })
+    //        .state("bar", {
+    //            url: "/bar",
+    //            template: '<div id="signpop" ng-include="\'/pages/sign_in.html\'"></div>'
+    //        });
+    //
+    //    $urlRouterProvider.otherwise("/bar");
+    //});
 
