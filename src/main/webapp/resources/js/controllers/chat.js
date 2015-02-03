acb6f7eba9f6f13e11472a35f71238fbae6acf5d
airofsummer <vulo4ka@gmail.com>
2015-02-03 22:43:52 +0200

    contacts, search

--- src/main/webapp/resources/js/controllers/chat.js
+++ src/main/webapp/resources/js/controllers/chat.js
@@ -1,43 +1,63 @@
 /**
  * Created by Airofsummer on 30.01.2015.
  */
-    angular.module('ChatApp', ['ui.bootstrap', "ui.router"]);
+    angular.module('ChatApp', ['ui.bootstrap', "ui.router", "ngAnimate"]);
 
-    angular.module('ChatApp').controller('Settings', function ($scope, $modal) {
+    angular.module('ChatApp').controller('Header', function ($scope, $modal) {
         $scope.open = function (size) {
 
             var modalInstance = $modal.open ({
                 templateUrl: 'settingsModal.html',
-                controller: 'ModalSettings',
+                controller: 'ModalChat',
                 size: size,
                 resolve: { },
                 backdrop: true
             });
 
         };
-
-    }).controller('Contacts', function ($scope, $modal) {
-            $scope.open = function (size) {
-
-                var modalInstance = $modal.open ({
-                    templateUrl: 'contactsModal.html',
-                    controller: 'ModalContacts',
-                    size: size,
-                    resolve: { },
-                    backdrop: true
-                });
-
-            };
-
-        }).controller("ChatApi", function($scope){
-        $scope.chats = [{
+        $scope.contactLists = [{
             title: 'Dark Daddy',
-            url: 'one.tpl.html'
+            url: 'force_gripper.tpl.html'
         }, {
             title: 'policebox',
-            url: 'two.tpl.html'
+            url: 'policebox.tpl.html'
+        }, {
+            title: 'sauron',
+            url: 'sauron.tpl.html'
+        }, {
+            title: 'Albus Percival Wulfric Brian Dumbledore',
+            url: 'albus.tpl.html'
+        }, {
+            title: 'Wenk Wenk',
+            url: 'wenk-wenk.tpl.html'
         }];
-        $scope.currentChat = 'one.tpl.html';
+        $scope.onClickContact = function (tab) {
+            $scope.currentContact = tab.url;
+        };
+
+        $scope.isActiveContact = function(chatUrl) {
+            return chatUrl == $scope.currentContact;
+        };
+        $scope.contacts = false;
+        $scope.add_cont = false;
+    }).controller("ChatApi", function($scope){
+        $scope.chats = [{
+            title: 'Dark Daddy',
+            url: 'force_gripper.tpl.html'
+        }, {
+            title: 'policebox',
+            url: 'policebox.tpl.html'
+        }, {
+            title: 'sauron',
+            url: 'sauron.tpl.html'
+        }, {
+            title: 'Albus Percival Wulfric Brian Dumbledore',
+            url: 'albus.tpl.html'
+        }, {
+            title: 'Wenk Wenk',
+            url: 'wenk-wenk.tpl.html'
+        }];
+        $scope.currentChat = 'force_gripper.tpl.html';
 
         $scope.onClickChat = function (tab) {
             $scope.currentChat = tab.url;
@@ -48,12 +68,20 @@
         };
     });
 
-    angular.module('ChatApp').controller('ModalSettings', function ($scope, $modalInstance) {
-        $scope.close = function () {
-            $modalInstance.close();
-        };
+var expectFriendNames = function(expectedNames, key) {
+    element.all(by.repeater(key + ' in chats').column(key + '.title')).then(function(arr) {
+        arr.forEach(function(wd, i) {
+            expect(wd.getText()).toMatch(expectedNames[i]);
+        });
     });
-    angular.module('ChatApp').controller('ModalContacts', function ($scope, $modalInstance) {
+    element.all(by.repeater(key + ' in contactLists').column(key + '.title')).then(function(arr) {
+        arr.forEach(function(wd, i) {
+            expect(wd.getText()).toMatch(expectedNames[i]);
+        });
+    });
+};
+
+    angular.module('ChatApp').controller('ModalChat', function ($scope, $modalInstance) {
         $scope.close = function () {
             $modalInstance.close();
         };
