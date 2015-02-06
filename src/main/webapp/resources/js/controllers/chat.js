99905ddb04aebc5610478f1e3db2916e0c1d1746
airofsummer <vulo4ka@gmail.com>
2015-02-06 19:34:50 +0200

    json front-end

--- src/main/webapp/resources/js/controllers/chat.js
+++ src/main/webapp/resources/js/controllers/chat.js
@@ -3,7 +3,8 @@
  */
     angular.module('ChatApp', ['ui.bootstrap', "ui.router", "ngAnimate"]);
 
-    angular.module('ChatApp').controller('Header', function ($scope, $modal) {
+    angular.module('ChatApp').controller('HeaderSettings', function ($scope, $modal, $http) {
+
         $scope.open = function (size) {
 
             var modalInstance = $modal.open ({
@@ -15,57 +16,40 @@
             });
 
         };
-        $scope.contactLists = [{
-            title: 'Dark Daddy',
-            url: 'force_gripper.tpl.html'
-        }, {
-            title: 'policebox',
-            url: 'policebox.tpl.html'
-        }, {
-            title: 'sauron',
-            url: 'sauron.tpl.html'
-        }, {
-            title: 'Albus Percival Wulfric Brian Dumbledore',
-            url: 'albus.tpl.html'
-        }, {
-            title: 'Wenk Wenk',
-            url: 'wenk-wenk.tpl.html'
-        }];
+
+
+
+    }).controller("ShowInfo", function($scope, $http){
+
+     $http.get("../pages/chats/settings.json")
+            .success(function(response) {$scope.settingsInfo = response;});
+
+    }).controller("HeaderContacts", function($scope, $http){
+
+        $http.get("../pages/chats/contacts.json")
+            .success(function(response) {$scope.contactLists = response;});
         $scope.onClickContact = function (tab) {
             $scope.currentContact = tab.url;
         };
-
         $scope.isActiveContact = function(chatUrl) {
             return chatUrl == $scope.currentContact;
         };
         $scope.contacts = false;
         $scope.add_cont = false;
-    }).controller("ChatApi", function($scope){
-        $scope.chats = [{
-            title: 'Dark Daddy',
-            url: 'force_gripper.tpl.html'
-        }, {
-            title: 'policebox',
-            url: 'policebox.tpl.html'
-        }, {
-            title: 'sauron',
-            url: 'sauron.tpl.html'
-        }, {
-            title: 'Albus Percival Wulfric Brian Dumbledore',
-            url: 'albus.tpl.html'
-        }, {
-            title: 'Wenk Wenk',
-            url: 'wenk-wenk.tpl.html'
-        }];
-        $scope.currentChat = 'force_gripper.tpl.html';
 
+    }).controller("ChatApi", function($scope, $http){
+
+        $http.get("../pages/chats/chatlist.json")
+            .success(function(response) {$scope.chats= response;});
+
+        $scope.currentChat = 'force_gripper.tpl.html';
         $scope.onClickChat = function (tab) {
             $scope.currentChat = tab.url;
         };
-
         $scope.isActiveChat = function(chatUrl) {
             return chatUrl == $scope.currentChat;
         };
+
     });
 
 var expectFriendNames = function(expectedNames, key) {
