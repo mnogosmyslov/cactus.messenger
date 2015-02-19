dfa9f4ae93dfe73f3acc305105d1118158c7cb55
airofsummer <vulo4ka@gmail.com>
2015-02-19 18:28:24 +0200

    chat fixes

--- src/main/java/org/cactus/messenger/controller/MainController.java
+++ src/main/java/org/cactus/messenger/controller/MainController.java
@@ -32,4 +32,8 @@ public class MainController {
     public String profileView() {
         return PageNames.PROFILE;
     }
+    @RequestMapping(value = "/chat", method = GET)
+    public String chatView() {
+        return PageNames.MESSENGER;
+    }
 }
