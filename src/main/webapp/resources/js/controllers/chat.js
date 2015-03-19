367fcda9589beeae7c655c8fd37b944663665a2d
airofsummer <vulo4ka@gmail.com>
2015-03-19 18:38:09 +0200

    profile load

--- src/main/webapp/resources/js/controllers/chat.js
+++ src/main/webapp/resources/js/controllers/chat.js
@@ -2,12 +2,10 @@
  * Created by Airofsummer on 30.01.2015.
  */
     angular.module('ChatApp', [
-
         "ui.router",
         "ngAnimate"]);
 
     angular.module('ChatApp').controller("HeaderContacts", function($scope, $http){
-
         $http.get("../cactus/pages/chats/contacts.json")
             .success(function(response) {$scope.contactLists = response;});
         $scope.onClickContact = function (tab) {
@@ -17,10 +15,11 @@
             return chatUrl == $scope.currentContact;
         };
         $scope.contacts = $scope.add_cont = $scope.setting = $scope.profile = false;
+        $scope.loadProfile = function () {
+            $http.get("/server/user/999")
+            .success(function(response) {$scope.settingsInfo = response; });
 
-        $http.get("../cactus/pages/chats/settings.json")
-            .success(function(response) {$scope.settingsInfo = response;});
-
+        }
 
 
     });
@@ -32,8 +31,10 @@ angular.module("ChatApp").controller("ChatApi", function($scope, $http, ChatServ
     $scope.history = [];
     $http.get("../cactus/pages/chats/chatid1.json")
         .success(function(response) {$scope.history = response;});
-        $http.get("../cactus/pages/chats/chatlist.json")
-            .success(function(response) {$scope.chats = response; $scope.currentChat = $scope.chats[0].url;});
+    $http.get("http://mydebug.hol.es/chatlist.php?userId=1000&authId=1QuVAyO8oFi2npXcGNB3KyAkkYz6liQm9khJT5Si")
+        .success(function(response) {$scope.chats = response; $scope.currentChat = $scope.chats[0].url;});
+    $http.get("http://mydebug.hol.es/contacts.php?userId=1000&authId=1QuVAyO8oFi2npXcGNB3KyAkkYz6liQm9khJT5Si")
+        .success(function(response) {$scope.profiles = response;});
 
         $scope.onClickChat = function (tab) {
             $scope.currentChat = tab.url;
