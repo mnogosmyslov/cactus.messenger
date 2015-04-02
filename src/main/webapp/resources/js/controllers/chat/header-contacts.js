5cd5fe3cca83692ef69fd5fff71dff05845d1915
airofsummer <vulo4ka@gmail.com>
2015-04-02 23:14:17 +0300

    ngRoute functionality

--- src/main/webapp/resources/js/controllers/chat/header-contacts.js
+++ src/main/webapp/resources/js/controllers/chat/header-contacts.js
@@ -1,18 +1,13 @@
 /**
  * Created by Airofsummer on 23.03.2015.
  */
-angular.module('ChatApp').controller("HeaderContacts", function($scope, $http){
-    $http.get("../cactus/pages/chats/contacts.json")
-        .success(function(response) {$scope.contactLists = response;});
+angular.module('ChatApp').controller("HeaderContacts", function($scope){
+
     $scope.onClickContact = function (tab) {
         $scope.currentContact = tab.url;
     };
     $scope.isActiveContact = function(chatUrl) {
         return chatUrl == $scope.currentContact;
     };
-    $scope.contacts = $scope.add_cont = $scope.setting = $scope.profile = false;
-    $scope.loadProfile = function () {
-        $http.get("/server/user/999")
-            .success(function(response) {$scope.settingsInfo = response; });
-    }
+
 });
--- src/main/webapp/resources/js/controllers/chat/route.js
+++ src/main/webapp/resources/js/controllers/chat/route.js
@@ -3,7 +3,21 @@
  */
 angular.module("ChatApp").config(function($routeProvider){
     $routeProvider.when('/new', {
-        templateUrl: '/newContact_frag.html',
-        controller: function(){alert("KKKKKEEEEKKK");}
-    });
+        templateUrl: '/cactus/pages/newContact_frag.html '
+    })
+        .when('/profile', {
+            templateUrl: '/cactus/pages/profile_frag.html ',
+            controller: function($http, $scope){
+                $http.get("/server/user/999")
+                    .success(function(response) {$scope.settingsInfo = response; });
+            }
+        })
+        .when('/contacts', {
+            templateUrl: '/cactus/pages/contacts_frag.html ',
+            controller: function($http, $scope){
+                $http.get("../cactus/pages/chats/contacts.json")
+                    .success(function(response) {$scope.contactLists = response;});
+            }
+        })
+        .otherwise({redirectTo: '/'});
 });
