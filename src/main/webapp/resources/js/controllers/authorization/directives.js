605de16033131bc29115cdfb4df0ccbe2f77caf5
airofsummer <vulo4ka@gmail.com>
2015-04-16 13:08:49 +0300

    demo

---
+++ src/main/webapp/resources/js/controllers/authorization/directives.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Airofsummer on 14.04.2015.
+ */
+angular.module('LandApp').directive('email', function() {
+    var EMAIL_REGEXP = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/i;
+
+    return {
+        require: 'ngModel',
+        restrict: '',
+        link: function(scope, elm, attrs, ctrl) {
+            // only apply the validator if ngModel is present and Angular has added the email validator
+            if (ctrl && ctrl.$validators.email) {
+
+                // this will overwrite the default Angular email validator
+                ctrl.$validators.email = function(modelValue) {
+                    return ctrl.$isEmpty(modelValue) || EMAIL_REGEXP.test(modelValue);
+                };
+            }
+        }
+    };
+});
