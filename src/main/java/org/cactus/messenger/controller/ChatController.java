d47bbbeac35d1d5c46d29c27a658cb5db205726d
AlexanderVoV <sashafox13@gmail.com>
2015-04-17 22:58:30 +0300

    Add UserAccount model in ChatController

--- src/main/java/org/cactus/messenger/controller/ChatController.java
+++ src/main/java/org/cactus/messenger/controller/ChatController.java
@@ -1,7 +1,14 @@
 package org.cactus.messenger.controller;
 
 import org.cactus.messenger.common.PageNames;
+import org.cactus.messenger.security.UserDetailsImpl;
+import org.cactus.share.service.UserAccountService;
+import org.cactus.share.vo.UserAccountVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -10,8 +17,14 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RequestMapping("/chat")
 public class ChatController {
 
+    @Autowired
+    private UserAccountService userAccountService;
+
     @RequestMapping(method = GET)
-    public String messengerView() {
+    public String messengerView(ModelMap modelMap) {
+        UserDetails userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserAccountVO userAccount = userAccountService.getUserWithoutPass(userDetails.getUsername());
+        modelMap.addAttribute("userAccount", userAccount);
         return PageNames.MESSENGER;
     }
 
